92dffe4c7a18e05827f066c8a82185df3e528fb6
NAkula011 <your-github-email@example.com>
2026-08-09 13:37:23 +0530

    Set Valid From to be Booking Date minus 2 days

--- src/components/BookingDetails.tsx
+++ src/components/BookingDetails.tsx
@@ -122,6 +122,7 @@ const BookingDetails = ({ ticketRecord, onEdit }: BookingDetailsProps) => {
   const bookedOn = `${String(bookedOnDate.getDate()).padStart(2, "0")}/${String(bookedOnDate.getMonth() + 1).padStart(2, "0")}/${bookedOnDate.getFullYear()} ${String(bookedOnDate.getHours()).padStart(2, "0")}:${String(bookedOnDate.getMinutes()).padStart(2, "0")}`;
 
   const validFromDate = new Date(bookedOnDate);
+  validFromDate.setDate(validFromDate.getDate() - 2);
   const validFrom = `${String(validFromDate.getDate()).padStart(2, "0")}/${String(validFromDate.getMonth() + 1).padStart(2, "0")}/${validFromDate.getFullYear()}`;
 
   const validTillDate = new Date(bookedOnDate);
